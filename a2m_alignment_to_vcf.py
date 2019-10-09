c19aa9a415e4771567873a22fad4b80e4fb26b52
Tuukka Norri <tsnorri@iki.fi>
2019-10-09 18:18:58 +0300

    Rewrite a2m_alignment_to_vcf.py

--- a2m_alignment_to_vcf.py
+++ a2m_alignment_to_vcf.py
@@ -1,5 +1,5 @@
 # 
-# Copyright (c) 2018 Tuukka Norri
+# Copyright (c) 2018-2019 Tuukka Norri
 # This code is licensed under MIT license (see LICENSE for details).
 # 
 
@@ -8,164 +8,136 @@ from Bio import SeqIO
 import sys
 
 
-def tr_rec_id(rec_id):
-	return rec_id.translate(str.maketrans("*:/", "_--"))
+def get_sequences(f):
+	seq_objs = [seq for seq in SeqIO.parse(f, "fasta")]
+	seq_ids = [seq.id for seq in seq_objs]
+	sequences = [seq.seq for seq in seq_objs]
+	return seq_ids, sequences
 
 
-def output_vcf_record(vcf, chrom, pos, rec_idx, ref, alt_ids, alt_seqs_by_id_):
-	assert 0 < len(ref)
+def format_ref(seq):
+	"""Remove gaps."""
+	return str(seq).replace('-', '')
+
+
+def format_nonempty_alt(ref, seq):
+	"""Remove gaps and replace N with REF values."""
+	assert len(ref) == len(seq)
+	retval = list(seq)
+	for i, c in enumerate(ref):
+		if 'N' == retval[i]:
+			retval[i] = c
+		# Check for an added gap character.
+		if '-' == retval[i]:
+			retval[i] = ''
+	retval = "".join(retval)
+	return retval
+
+
+def format_alt(ref, seq):
+	"""Return <DEL> for empty ALTs, otherwise call format_nonempty_alt."""
+	for c in seq:
+		if '-' != c:
+			return format_nonempty_alt(ref, seq)
+	return "<DEL>"
+
+
+def format_sample_name(n):
+	"""Remove unusual characters."""
+	return n.translate(str.maketrans("*:/", "_--"))
+
+
+def handle_range(base_pos, chrom, sequences, seq_ids, rs, re, join_gt_by):
+	ref, *rest = sequences
+	ref_part = ref[rs:re]
+	ref_part_formatted = format_ref(ref_part)
+	assert not 'N' in ref_part_formatted
 	
-	# Map ALT sequences to sequence identifiers that have an ALT.
-	# Also process the sequences by removing '-'.
-	alt_ids_by_seq = {}
-	alt_seqs_by_id = {}
-	for seq_id, seq in alt_seqs_by_id_.items():
-		new_seq = "".join(filter(lambda c: '-' != c, seq))
-		new_seq = new_seq if 0 < len(new_seq) else '<DEL>'
-		
-		alt_ids_by_seq.setdefault(new_seq, set()).add(seq_id)
-		alt_seqs_by_id[seq_id] = new_seq
-	
-	## Debug
-	#for k, v in alt_seqs_by_id_.items():
-	#	sys.stdout.write("".join(v))
-	#print("")
-	#for k, v in alt_ids_by_seq.items():
-	#	print("k: %s" % k)
-	#	print("v: %d" % len(v))
-	#	#for val in v:
-	#	#	print("v: %s" % v)
-	#sys.exit(0)
-	
-	# Map ALT sequences to indices.
-	alt_seqs = [seq for seq in alt_ids_by_seq.keys()]
-	alt_idxs_by_seq = { seq: i for i, seq in enumerate(alt_seqs) }
+	# Format each ALT.
+	alts = [format_alt(ref_part, seq[rs:re]) for seq in rest]
 
-	# Output a record.
-	vcf.write(str(chrom))				# CHROM
-	vcf.write("\t")
-	vcf.write(str(pos))					# POS
-	vcf.write("\t")
-	vcf.write("p%d" % rec_idx)			# ID
-	vcf.write("\t")
-	vcf.write("".join(ref))				# REF
-	vcf.write("\t")
-	vcf.write(",".join(alt_seqs))		# ALT
-	vcf.write("\t")
-	vcf.write(".")						# QUAL
-	vcf.write("\t")
-	vcf.write(".")						# FILTER
-	vcf.write("\t")
-	vcf.write(".")						# INFO
-	vcf.write("\t")
-	vcf.write("GT")						# FORMAT
-	
-	for alt_id in alt_ids:
-		vcf.write("\t")
-		alt_idx = 0
-		if alt_id in alt_seqs_by_id:
-			alt_seq = alt_seqs_by_id[alt_id]
-			alt_idx = alt_idxs_by_seq[alt_seq]
-			
-		vcf.write(str(alt_idx))
+	# Get the unique values.
+	unique_alts_without_ref = frozenset(filter(lambda x: x != ref_part_formatted, alts))
+	if 0 == len(unique_alts_without_ref):
+		return
 
-	vcf.write("\n")
+	# Assign indices to them.
+	alt_idxs = {alt: 1 + i for i, alt in enumerate(unique_alts_without_ref)}
+
+	# Get the unique values in the order just determined.
+	alts_in_order = [x[0] for x in sorted(alt_idxs.items(), key = lambda kv: kv[1])]
+
+	# Add REF to alt_idxs.
+	alt_idxs[ref_part_formatted] = 0
+
+	# CHROM POS ID REF ALT QUAL FILTER INFO FORMAT
+	print("%d\t%d\t.\t%s\t%s\t.\tPASS\t.\tGT\t%s" % (chrom, base_pos + rs, ref_part_formatted, ",".join(alts_in_order), join_gt_by.join([str(alt_idxs[alt]) for alt in alts])))
 
 
-# Parse the command line arguments.
-parser = argparse.ArgumentParser(description = "Convert A2M multialignment to VCF and FASTA.")
-parser.add_argument("--chrom", required = True, type = int)
-parser.add_argument('--fasta', required = True, type = argparse.FileType('w'))
-parser.add_argument('--vcf', required = True, type = argparse.FileType('w'))
-
+parser = argparse.ArgumentParser(description = "Transform a multiple alignment in A2M format into variants. The first sequence needs to be the reference.")
+parser.add_argument('--input', type = argparse.FileType('r'), required = True, help = "Input MSA")
+parser.add_argument('--chr', type = int, required = True, help = "Chromosome number")
+parser.add_argument('--base-position', type = int, default = 0, help = "Base position to be added to the co-ordinates")
+parser.add_argument('--mangle-sample-names', action = 'store_true', help = "Replace unusual characters in sample names")
+parser.add_argument('--specific-type', nargs = '*', type = str, default = [], action = "store", help = "Instead of writing one haploid sample for each HLA type, output one diploid donor with the given HLA types.")
 args = parser.parse_args()
-chrom = args.chrom
-fasta = args.fasta
-vcf = args.vcf
 
-# Read the sequences.
-sequences = []
-rec_ids = []
-for record in SeqIO.parse(sys.stdin, "fasta"):
-	sequences.append(record)
-	rec_id = record.id
+if len(args.specific_type) not in (0, 2):
+	print(len(args.specific_type))
+	print("Exactly zero or two --specific-type arguments needed.", file = sys.stderr)
+	sys.exit(1)
 
-	# Translate characters for use in file names (though some of these might be allowed by the FS).
-	rec_id = tr_rec_id(rec_id)
-	rec_ids.append(rec_id)
+seq_ids, sequences = get_sequences(args.input)
 
-# Output VCF header.
-vcf.write("##fileformat=VCFv4.2\n")
-vcf.write("##source=a2m-alignment-to-vcf.py\n")
-
-# Output the format header.
-vcf.write("#CHROM\tPOS\tID\tREF\tALT\tQUAL\tFILTER\tINFO\tFORMAT")
-for rec_id in rec_ids:
-	vcf.write("\t")
-	vcf.write(rec_id)
-vcf.write("\n")
-
-# Output the first sequence without the gaps.
-gap_indices = []
-first_record = sequences[0]
-ref = first_record.seq
-fasta.write(">")
-fasta.write(first_record.id)
-fasta.write("\n")
-for i, c in enumerate(ref):
-	if '-' == c:
-		gap_indices.append(i)
-	else:
-		fasta.write(c)
-fasta.write("\n")
-gap_indices = frozenset(gap_indices)
-
-# Output the VCF records.
-insertions = 0
-records = 0
-current_rec_pos = 0
-current_ref = []
-current_alts = {}
-for i, rc in enumerate(ref):
+# Check for gap characters in the first column.
+for s in sequences:
+	if '-' == s[0]:
+		print("Found a gap character in the first column.", file = sys.stderr)
+		sys.exit(1)
 	
-	# VCF recuires us to have a reference sequence for each insertion. Hence, do the following steps:
-	# - Suppose that we're at index i. Maintain the reference sequence and ALTs at index i - 1.
-	# - If ref[i] != '-', output the previous ALTs (in current_alts) and reset the variables.
-	# - Otherwise, append the non-matching characters in to the ALT sequences.
-	# - The case where position 1 contains a polymorphism is not handled.
+# Make a bit vector of positions that are followed by a gap.
+gap_follows = len(sequences[0]) * [False]
+for i, c in enumerate(sequences[0][1:]):
+	gap_follows[i] = ('-' == c)
 
-	if '-' == rc:
-		insertions += 1
-	else:
-		if 0 < len(current_alts):
-			records += 1
-			output_vcf_record(vcf, chrom, 1 + current_rec_pos, records, current_ref, rec_ids, current_alts)
-			
-		current_rec_pos = i - insertions
-		current_ref = [rc]
-		current_alts = {}
+# Filter by --specific-type.
+join_gt_by = "\t"
+if 0 != len(args.specific_type):
+	join_gt_by = "|"
+	lhs_seq_idx = None
+	rhs_seq_idx = None
+	try:
+		lhs_seq_idx = 1 + seq_ids[1:].index(args.specific_type[0])
+	except ValueError:
+		print("Sequence with id %s not found." % args.specific_type[0], file = sys.stderr)
+		sys.exit(1)
+	try:
+		rhs_seq_idx = 1 + seq_ids[1:].index(args.specific_type[1])
+	except ValueError:
+		print("Sequence with id %s not found." % args.specific_type[1], file = sys.stderr)
+		sys.exit(1)
+	seq_ids = [seq_ids[0], seq_ids[lhs_seq_idx], seq_ids[rhs_seq_idx]]
+	sequences = [sequences[0], sequences[lhs_seq_idx], sequences[rhs_seq_idx]]
+
+# Output the VCF header.
+formatted_sample_names = None
+if args.mangle_sample_names:
+	formatted_sample_names = [format_sample_name(x) for x in seq_ids[1:]]
+	assert len(formatted_sample_names) == len(frozenset(formatted_sample_names))
+else:
+	formatted_sample_names = seq_ids[1:]
+
+print("##fileformat=VCFv4.2")
+print('##FORMAT=<ID=GT,Number=1,Type=String,Description="Genotype">')
+print("CHROM\tPOS\tID\tREF\tALT\tQUAL\tFILTER\tINFO\tFORMAT\t%s" % ("\t".join(formatted_sample_names) if 0 == len(args.specific_type) else "SAMPLE1"))
+
+# Range start, end.
+rs = 0
+re = 0
+for i, next_is_gap in enumerate(gap_follows):
+	re = i + 1
+	if next_is_gap:
+		continue
 	
-	# Check if the reference differs from any of the sequences
-	# and update current_alts accordingly.
-	have_diff = False
-	it = iter(sequences)
-	next(it) # Skip the reference.
-	for record in it:
-		rec_id = tr_rec_id(record.id)
-		ac = record.seq[i]
-		seq = None
-		if ac != rc:
-			have_diff = True
-
-			# Append ac to the ALTs.
-			if rec_id in current_alts:
-				seq = current_alts[rec_id]
-			else:
-				seq = current_ref[:-1]	# Slice up to but not including the last element.
-				current_alts[rec_id] = seq
-			seq.append(ac)
-
-# Output the final record if needed.
-if 0 < len(current_alts):
-	records += 1
-	output_vcf_record(vcf, chrom, 1 + current_rec_pos, records, current_ref, rec_ids, current_alts)
+	handle_range(args.base_position, args.chr, sequences, seq_ids, rs, re, join_gt_by)
+	rs = re
