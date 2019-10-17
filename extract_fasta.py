90543346b6088b2e731f9d36aaab66b3ce5a679e
Tuukka Norri <tsnorri@iki.fi>
2019-10-17 14:49:05 +0300

    Add outputting reverse complement

--- extract_fasta.py
+++ extract_fasta.py
@@ -15,6 +15,8 @@ from Bio import SeqIO
 parser = argparse.ArgumentParser("Output the specified sequence in a FASTA file.")
 parser.add_argument('--fasta', required = True, type = argparse.FileType('rU'))
 parser.add_argument('--seq-id', required = True)
+parser.add_argument('--reverse-complement', action = "store_true")
+parser.add_argument('--remove-gaps', action = "store_true")
 args = parser.parse_args()
 
 print("Reading FASTA…", file = sys.stderr)
@@ -23,9 +25,18 @@ for rec in SeqIO.parse(args.fasta, format = "fasta"):
 	print("Found record: %s" % rec.id, file = sys.stderr)
 	if rec.id == args.seq_id:
 		print(">%s" % rec.id)
-		# FIXME: this is really inefficient.
-		for i in range(len(rec.seq)):
-			sys.stdout.write(rec.seq[i])
+		if args.reverse_complement:
+			table = {'A': 'T', 'C': 'G', 'G': 'C', 'T': 'A', '-': '-', 'N': 'N'}
+			for i in reversed(range(len(rec.seq))):
+				if args.remove_gaps and '-' == rec.seq[i]:
+					continue
+				sys.stdout.write(table[rec.seq[i]])
+		else:
+			# FIXME: this is really inefficient.
+			for i in range(len(rec.seq)):
+				if args.remove_gaps and '-' == rec.seq[i]:
+					continue
+				sys.stdout.write(rec.seq[i])
 		sys.stdout.write("\n")
 		sys.exit(0)
 
