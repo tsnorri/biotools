c4b08cc5084940a8948e2f594daf0c4269a0bca1
Tuukka Norri <tsnorri@iki.fi>
2019-10-17 14:48:10 +0300

    Add filtering by chromosome

--- compare_variant_file_to_reference.py
+++ compare_variant_file_to_reference.py
@@ -14,17 +14,21 @@ if __name__ == "__main__":
 	parser = argparse.ArgumentParser(description = "Check the REF column of a variant file.")
 	parser.add_argument('--vcf', type = argparse.FileType('r'), help = "Variant file")
 	parser.add_argument('--reference', type = argparse.FileType('r'), help = "Reference FASTA")
+	parser.add_argument('--chr', type = str, required = False, default = None, help = "Filter by chromosome")
 	args = parser.parse_args()
 	
 	# Take the first sequence.
 	fasta_record = next(SeqIO.parse(args.reference, format = "fasta"))
+	print("Using sequence with ID '%s' as the reference." % fasta_record.id, file = sys.stderr)
 	reference_seq = fasta_record.seq
 	
 	vcf_reader = vcf.Reader(args.vcf)
 	for row in vcf_reader:
+		if not (args.chr is None or row.CHROM == args.chr):
+			continue
+
 		pos = row.POS
-		
-		actual = row.REF
-		expected = reference_seq[pos - 1 : pos - 1 + len(actual)]
+		actual = str(row.REF).upper()
+		expected = str(reference_seq[pos - 1 : pos - 1 + len(actual)]).upper()
 		if actual != expected:
 			print("Mismatch at VCF position %d: expected '%s', got '%s'" % (pos, expected, actual), file = sys.stderr)
